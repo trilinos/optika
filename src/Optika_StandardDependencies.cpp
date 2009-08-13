904464969f87149329d6e34aaf099ae19f2135a7
Kurtis L. Nusbaum <klnusbaum@csbsju.edu>
2009-08-13 17:26:40 +0000

    Added copyright headers to all sourcecode files -Kurtis

--- src/Optika_StandardDependencies.cpp
+++ src/Optika_StandardDependencies.cpp
@@ -1,3 +1,30 @@
+// @HEADER
+// ***********************************************************************
+// 
+//         Optika: A Tool For Developing Parameter Obtaining GUIs
+//                Copyright (2009) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Kurtis Nusbaum (klnusbaum@gmail.com) 
+// 
+// ***********************************************************************
+// @HEADER
 #include "Optika_StandardDependencies.hpp"
 #include <QDir>
 
@@ -113,7 +140,7 @@ template <class S>
 void NumberArrayLengthDependency::modifyArrayLength(unsigned int newLength){
 	const Teuchos::Array<S> originalArray = Teuchos::any_cast<Teuchos::Array<S> >(dependent->getAny()); 
 	Teuchos::RCP<const EnhancedNumberValidator<S> > potentialValidator = Teuchos::null;
-	if(!dependent->validator().is_null()){
+	if(!Teuchos::is_null(dependent->validator())){
 		potentialValidator = Teuchos::rcp_dynamic_cast<const ArrayNumberValidator<S> >(dependent->validator(),true)->getPrototype();
 	}
 	Teuchos::Array<S> newArray;
@@ -122,7 +149,7 @@ void NumberArrayLengthDependency::modifyArrayLength(unsigned int newLength){
 		newArray.append(originalArray[i]);
 	}
 	for(;i<newLength;i++){
-			if(potentialValidator.is_null()){
+			if(Teuchos::is_null(potentialValidator)){
 				newArray.append(0);
 			}
 			else{
